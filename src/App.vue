3bd66bae9092ea7ea49591c0a2f5f3abe4f8cdb8
nanguaZYL <2319782710@qq.com>
2020-08-14 18:15:36 +0800

    上传项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
+<style lang='less'>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+ul {
+  list-style: none;
+}
+.el-form-item__content {
+  margin-left: 0 !important;
+  .upload-demo {
+    display: flex;
+  }
+  ul {
+    display: flex;
+    li {
+      width: 0;
+      margin: 5px !important;
+    }
+  }
 }
 </style>
